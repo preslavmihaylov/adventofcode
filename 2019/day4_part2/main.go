3e2eb77b20dc04f3fe8792f7554ba7357f02c4a9
Preslav Mihaylov <pmihay@ext.uber.com>
2019-12-04 07:48:05 +0200

    added 2019, day4 solutions

---
+++ 2019/day4_part2/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	cnt := 0
+	for i := 264793; i < 803935; i++ {
+		if meetsCriteria(strconv.Itoa(i)) {
+			fmt.Println(i)
+			cnt++
+		}
+	}
+
+	fmt.Println(cnt)
+}
+
+func meetsCriteria(password string) bool {
+	hasDouble := false
+	biggestDigit := 0
+	for i := range password {
+		if (i < len(password)-1 && password[i] == password[i+1]) &&
+			!((i > 0 && password[i] == password[i-1]) || (i < len(password)-2 && password[i] == password[i+2])) {
+			hasDouble = true
+		}
+
+		digit, err := strconv.Atoi(string(password[i]))
+		if err != nil {
+			panic(err)
+		}
+
+		if biggestDigit > digit {
+			return false
+		}
+
+		biggestDigit = digit
+	}
+
+	return hasDouble
+}
