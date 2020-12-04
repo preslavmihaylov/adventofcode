5d17ef38536770b08002ea75b2085e8a5dd9ff30
preslavmihaylov <preslav@pmihaylov.com>
2020-12-04 18:29:56 +0200

    resolved race condition in day 01 part 01 of adventofcode

--- 2020/day01/part01/src/com/pmihaylov/Main.java
+++ 2020/day01/part01/src/com/pmihaylov/Main.java
@@ -4,26 +4,38 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         String[] lines = readInput();
-        Set<Integer> seenNums = ConcurrentHashMap.newKeySet();
+        ConcurrentHashMap<Integer, Boolean> seenNums = new ConcurrentHashMap<>();
         ParallelTraverser<Integer> traverser = new ParallelTraverser<>();
         try {
             Optional<Integer> result = traverser.traverse(lines.length, (r) -> r != -1, (start, end) -> () -> {
                 for (int i = start; i < end; i++) {
                     int firstNum = Integer.parseInt(lines[i]);
                     int target = 2020 - firstNum;
-                    if (seenNums.contains(target)) {
-                        System.out.printf("Finished calculating [%s;%s)\n", start, end);
-                        return target * firstNum;
-                    }
 
-                    seenNums.add(firstNum);
+                    AtomicInteger r = new AtomicInteger(-1);
+                    seenNums.compute(firstNum, (k, v) -> {
+                        if (seenNums.containsKey(target)) {
+                            System.out.printf("Finished calculating [%s;%s)\n", start, end);
+                            r.set(target * firstNum);
+                        }
+
+                        return true;
+                    });
+
+                    if (r.get() != -1) {
+                        return r.get();
+                    }
                 }
 
                 System.out.printf("Finished calculating [%s;%s)\n", start, end);
