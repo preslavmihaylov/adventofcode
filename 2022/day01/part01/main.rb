02e42d3bdaf0492ceae802df6e1288c8bc1eeddc
pmihaylov_bp <preslav.mihaylov@bottlepay.com>
2022-12-01 20:47:44 +0200

    add day01 solution

---
+++ 2022/day01/part01/main.rb
@@ -0,0 +1,24 @@
+#!/bin/usr/env ruby
+
+def read_input(filename = "input.txt")
+  calories_per_elf = []
+  calories_per_elf.append([])
+
+  data = File.open(filename).read
+  data.lines.each do |line|
+    if line.strip.empty?
+      calories_per_elf.append([])
+      next
+    end
+
+    calories_per_elf.last.append(line.to_i)
+  end
+
+  return calories_per_elf
+end
+
+if __FILE__ == $0
+  calories_per_elf = read_input()
+  max_calories = calories_per_elf.max_by { |calories| calories.sum }
+  puts max_calories.sum
+end
