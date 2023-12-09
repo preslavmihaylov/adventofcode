5a291e57886efdc85cb0be2cac145b0b10bf7cf4
preslavmihaylov <preslav@pmihaylov.com>
2023-12-09 12:02:32 +0200

    add day09 solutions

---
+++ 2023/day09/part01/index.ts
@@ -0,0 +1,35 @@
+const input = await Bun.file("input.txt").text();
+const sequences = input
+  .split("\n")
+  .filter((line) => line.length > 0)
+  .map((line) => line.split(" ").map((el) => parseInt(el)));
+
+let sum = 0;
+for (const sequence of sequences) {
+  const derivatives: number[][] = [sequence];
+  let currList = sequence;
+  while (!currList.every((el) => el === 0)) {
+    const nextList = [];
+    for (let i = 0; i < currList.length - 1; i++) {
+      nextList.push(currList[i + 1] - currList[i]);
+    }
+
+    currList = nextList;
+    derivatives.push(currList);
+  }
+
+  const result = extrapolate(derivatives);
+  sum += result;
+}
+
+console.log(sum);
+
+function extrapolate(derivatives: number[][]): number {
+  const lastDerivative = derivatives[derivatives.length - 1];
+  let curr = lastDerivative[lastDerivative.length - 1];
+  for (let i = derivatives.length - 2; i >= 0; i--) {
+    curr = derivatives[i][derivatives[i].length - 1] + curr;
+  }
+
+  return curr;
+}
