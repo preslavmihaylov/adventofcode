d9056fbc8d08c8ccb73933c8eaa96f2fa6cdbc41
preslavmihaylov <preslav@pmihaylov.com>
2024-12-01 14:58:59 +0100

    refactor

--- 2024/day01/part02/main.hs
+++ 2024/day01/part02/main.hs
@@ -1,26 +1,17 @@
-import Data.List (sort)
-
 main = do
   input <- readFile "../input.txt"
   print $ solve $ mapInput input
 
-solve arrs = sum $ map (\x -> x * (totalOccurrences x rightArr)) leftArr
+solve arrs = sum $ map (\x -> x * (totalOccurrences rightArr x)) leftArr
   where
     leftArr = fst arrs
     rightArr = snd arrs
 
-totalOccurrences el elements = sum [1 | x <- elements, x == el]
-
--- calcDistance arrs = if null leftArr then 0 else currDist + calcDistance (tail leftArr, tail rightArr)
---   where
---     leftArr = fst arrs
---     rightArr = snd arrs
---     currDist = abs ((head leftArr) - (head rightArr))
+totalOccurrences elements el = sum [1 | x <- elements, x == el]
 
 mapInput input = (leftArr, rightArr)
   where 
-    ls = lines input
-    tokens = map splitLine ls
+    tokens = map splitLine $ lines input
     leftArr = [read (x !! 0) :: Int | x <- tokens]
     rightArr = [read (x !! 1) :: Int | x <- tokens]
 
